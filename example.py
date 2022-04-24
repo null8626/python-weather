5aed91f7b46cca54b6a8b761028aa31b2389dbbe
null <60427892+vierofernando@users.noreply.github.com>
2022-04-24 13:48:05 +0700

    fix: support for python 3.10 or later

--- example.py
+++ example.py
@@ -20,5 +20,5 @@ async def getweather():
     await client.close()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop() # for Python >= 3.10, otherwise use get_event_loop
     loop.run_until_complete(getweather())
