40e7e4ec2397241be7b8cea4b2353b00bfbc6a21
null <60427892+null8626@users.noreply.github.com>
2023-03-17 20:22:08 +0700

    refactor: use the constants [skip ci]

--- python_weather/constants.py
+++ python_weather/constants.py
@@ -8,7 +8,7 @@ UTC_DATETIME_REGEX = re.compile(r'(\d{2})\:(\d{2}) (A|P)M$')
 DATE_REGEX = re.compile(r'^(\d{4})\-(\d{2})\-(\d{2})$')
 TIME_REGEX = re.compile(r'^(\d{2})\:(\d{2}) (A|P)M$')
 LATLON_REGEX = re.compile(r'^Lat ([\d\-\.]+) and Lon ([\d\-\.]+)$')
-VALID_FORMATS = ('F', 'C')
+VALID_FORMATS = (METRIC, IMPERIAL)
 
 is_invalid_format = (
   lambda fmt: not fmt or not isinstance(fmt, str) or fmt.upper() not in VALID_FORMATS
