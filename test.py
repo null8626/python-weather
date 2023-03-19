fc559f5d5e4f1df3f3e9cd27a38623a7775df805
null8626 <vierofernando9@gmail.com>
2023-03-19 14:15:19 +0700

    style: format test.py

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ async def getweather():
   async with python_weather.Client(format=python_weather.IMPERIAL) as client:
     # fetch a weather forecast from a city
     weather = await client.get('New York')
-
+    
     # returns the current day's forecast temperature (int)
     print(weather.current.temperature)
-
+    
     # get the weather forecast for a few days
     for forecast in weather.forecasts:
       print(forecast)
-
+      
       # hourly forecasts
       for hourly in forecast.hourly:
         print(f' --> {hourly!r}')
@@ -25,5 +25,5 @@ if __name__ == '__main__':
   # for more details
   if os.name == 'nt':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
+  
   asyncio.run(getweather())
