2d0e87e21cb956af86df6e9d4ae02e6bf5d77174
null8626 <vierofernando9@gmail.com>
2023-04-16 17:14:35 +0700

    doc: __repr__ improvements

--- python_weather/enums.py
+++ python_weather/enums.py
@@ -270,6 +270,11 @@ class Kind(Enum):
       return self.HEAVY_SNOW_SHOWERS
     elif num == 200:
       return self.THUNDERY_SHOWERS
+
+  def __repr__(self) -> str:
+    """:class:`str`: The string representation of this object."""
+    
+    return f'<{self.__class__.__name__}.{self.name} emoji={self.emoji!r}>'
   
   def __str__(self) -> str:
     """:class:`str`: The stylized name for this :class:`Enum`."""
@@ -328,6 +333,11 @@ class Phase(Enum):
   WANING_GIBBOUS = 'Waning Gibbous'
   LAST_QUARTER = 'Last Quarter'
   WANING_CRESCENT = 'Waning Crescent'
+  
+  def __repr__(self) -> str:
+    """:class:`str`: The string representation of this object."""
+    
+    return f'<{self.__class__.__name__}.{self.name} emoji={self.emoji!r}>'
   
   def __str__(self) -> str:
     """:class:`str`: The stylized name for this :class:`Enum`."""
