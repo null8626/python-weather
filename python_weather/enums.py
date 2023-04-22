bac7f05b79dd23ba17a6d4b0130883be6b253e10
null8626 <vierofernando9@gmail.com>
2023-04-22 23:37:41 +0700

    ci: better test.py

--- python_weather/enums.py
+++ python_weather/enums.py
@@ -279,11 +279,6 @@ class Kind(Enum):
     elif num == 200:
       return self.THUNDERY_SHOWERS
   
-  def __repr__(self) -> str:
-    """:class:`str`: The string representation of this object."""
-    
-    return f'<{self.__class__.__name__}.{self.name} emoji={self.emoji!r}>'
-  
   def __str__(self) -> str:
     """:class:`str`: The stylized name for this :class:`Enum`."""
     
@@ -334,9 +329,9 @@ class Kind(Enum):
 
 class Phase(Enum):
   """Represents a moon phase."""
-  
+
   __slots__ = ()
-  
+
   NEW_MOON = 'New Moon'
   WAXING_CRESCENT = 'Waxing Crescent'
   FIRST_QUARTER = 'First Quarter'
@@ -345,11 +340,6 @@ class Phase(Enum):
   WANING_GIBBOUS = 'Waning Gibbous'
   LAST_QUARTER = 'Last Quarter'
   WANING_CRESCENT = 'Waning Crescent'
-  
-  def __repr__(self) -> str:
-    """:class:`str`: The string representation of this object."""
-    
-    return f'<{self.__class__.__name__}.{self.name} emoji={self.emoji!r}>'
   
   def __str__(self) -> str:
     """:class:`str`: The stylized name for this :class:`Enum`."""
