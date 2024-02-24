e43eb9a81d246dc9d2cfb0565baf163d7a5e82d8
null <60427892+null8626@users.noreply.github.com>
2024-02-24 11:54:25 +0700

    fix: fix incorrect enum capitalization

--- python_weather/enums.py
+++ python_weather/enums.py
@@ -458,7 +458,7 @@ class Phase(BasicEnum):
   NEW_MOON = 'New Moon'
   WAXING_CRESCENT = 'Waxing Crescent'
   FIRST_QUARTER = 'First Quarter'
-  Waxing_GIBBOUS = 'Waxing Gibbous'
+  WAXING_GIBBOUS = 'Waxing Gibbous'
   FULL_MOON = 'Full Moon'
   WANING_GIBBOUS = 'Waning Gibbous'
   LAST_QUARTER = 'Last Quarter'
