51eb7f7e5b6410d21ddf784b642199767aa77f28
null8626 <vierofernando9@gmail.com>
2024-02-25 00:29:10 +0700

    doc: documentation tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,5 @@ extensions = [
   'sphinx.ext.intersphinx'
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 html_theme = 'furo'
-#html_static_path = ['_static']
+html_title = 'python-weather'
--- python_weather/errors.py
+++ python_weather/errors.py
@@ -1,4 +1,4 @@
 class Error(Exception):
-  """Represents a ``python_weather`` error class. Extends python's :py:class:`Exception` class."""
+  """Represents a ``python_weather`` error class. Extends :py:class:`Exception`."""
   
   __slots__ = ()
