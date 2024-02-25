8bbce8bf78ff2cb8720582ae4e43d8cef05c9a22
null8626 <vierofernando9@gmail.com>
2024-02-25 14:59:56 +0700

    doc: remove injected divs

--- docs/_static/script.js
+++ docs/_static/script.js
@@ -1,6 +1,6 @@
 try {
   // remove this useless crap that appears on official readthedocs builds
-  document.querySelector('#furo-readthedocs-versions').remove()
+  document.querySelector('.injected').remove()
 } catch {}
 
 if (document.querySelector('section#python-weather')) {
