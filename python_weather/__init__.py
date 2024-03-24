2e4fe64fa9062d7fd5892a55a422c9eca7be892c
null8626 <vierofernando9@gmail.com>
2024-03-25 05:00:02 +0700

    2.0.1 [skip ci]

--- python_weather/__init__.py
+++ python_weather/__init__.py
@@ -1,13 +1,9 @@
-"""
-python-weather
-"""
-
 from .enums import HeatIndex, Kind, Locale, Phase, UltraViolet, WindDirection
 from .constants import METRIC, IMPERIAL
 from .client import Client
 from .errors import Error
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __all__ = (
   'METRIC',
   'IMPERIAL',
